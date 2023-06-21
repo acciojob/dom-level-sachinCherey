5e9455116855c148574ae2cd77c196bd77162de8
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-06-21 16:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let eleId="level";
+function findlevel(eleId) {
+	let ele=document.getElementById(eleId);
+	let Dlevel=0;
+	let Cnode=ele;
+	while(Cnode.parentNode){
+		Dlevel++;
+		Cnode=Cnode.parentNode;
+	}
+
+	alert(`The level of the element is:${Dlevel}`);
+}
+
+findlevel(eleId);
+
